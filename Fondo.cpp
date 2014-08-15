15af4a5d87b93352fe068f39743d2d298a0dadb7
Sebas95 <sgonzalezquesada@gmail.com>
2014-08-14 23:57:24 -0600

    Prueba commit de Sebas95

---
+++ Fondo.cpp
@@ -0,0 +1,119 @@
+/*
+ * AreaDeJuego.cpp
+ *
+ *  Created on: 08/08/2014
+ *      Author: sebastian
+ */
+#include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
+
+#include "Fondo.h"
+#include <SDL/SDL.h>
+
+
+Fondo::Fondo() {
+
+	rect_fondo.x = 0;
+	rect_fondo.y = 0;
+	rect_fondo.h = NULL;
+	rect_fondo.w = NULL;
+	rect_fondo2.x = 0;
+	rect_fondo2.y = 0;
+	rect_fondo2.h = NULL;
+	rect_fondo2.w = NULL;
+
+
+}
+
+Fondo::~Fondo() {
+	// TODO Auto-generated destructor stub
+}
+
+void Fondo::dibujarFondo(){
+	SDL_BlitSurface(imagen_fondo,NULL,screen, &rect_fondo2);
+	SDL_BlitSurface(imagen_fondo,NULL,screen, &rect_fondo);
+
+
+}
+void Fondo::actualizarFondo(){
+
+	SDL_Flip(screen);
+
+}
+void Fondo::cargarImagenFondo(){
+	imagen_fondo = SDL_LoadBMP("fondo.bmp");
+}
+int Fondo::dibujarVentana(){
+
+
+	        if (SDL_Init(SDL_INIT_VIDEO) == -1)
+	        {
+	                printf("Error en SDL_Init: %s\n", SDL_GetError());
+	                return 1;
+	        }
+
+
+
+	        screen = SDL_SetVideoMode(ANCHO_DE_PANTALLA,LARGO_DE_PANTALLA, 16, SDL_ANYFORMAT);
+
+	        if (screen == NULL)
+	        {
+	                printf("Error en SDL_SetVideoMode: %s\n", SDL_GetError());
+	                return 1;
+	        }
+
+	        SDL_WM_SetCaption("Crazy Defender", NULL);
+
+
+}
+void Fondo::rellenarFondoNegro(){
+	SDL_FillRect(screen , 0 , SDL_MapRGB(screen->format,0,0,0));
+}
+
+void Fondo::init(){
+	SDL_Init(SDL_INIT_EVERYTHING);
+}
+ void Fondo::moverRectFondo_x(int num_de_pixeles){
+     p1 = p1 + num_de_pixeles;
+	 rect_fondo.x =  p1;
+
+ }
+
+ int Fondo::getPosicionRectFondo_x(){
+ 	 return rect_fondo.x;
+ }
+ void Fondo::moverRect2Fondo_x(int num_de_pixeles){
+	 p2 = p2 + num_de_pixeles;
+ 	 rect_fondo2.x =  p2;
+
+  }
+
+  int Fondo::getPosicionRect2Fondo_x(){
+  	 return rect_fondo2.x;
+  }
+
+  void Fondo::mover(int velocidad){
+	  moverRect2Fondo_x(velocidad);
+	  moverRectFondo_x(velocidad);
+	  if (p1 >= ANCHO_DE_PANTALLA*2 ){
+		  p1 = INICIAL_P2  ;
+	  }
+	  if (p2 >= ANCHO_DE_PANTALLA*2){
+	  		  p2 = INICIAL_P2;
+	  	  }
+	 /* if (p1 <= INICIAL_P2 ){
+	  		  p1 = ANCHO_DE_PANTALLA*2   ;
+	  	  }
+	  if (p2 <= INICIAL_P2){
+	  		 p2 = ANCHO_DE_PANTALLA*2 ;
+	  	  }*/
+
+  }
+
+
+
+
+
---
+++ Fondo.h
@@ -0,0 +1,58 @@
+/*
+ * AreaDeJuego.h
+ *
+ *  Created on: 08/08/2014
+ *      Author: sebastian
+ */
+#include <iostream>
+#include <string>
+using std::string;
+
+
+#include <SDL/SDL.h>
+using std::cout;
+using std::endl;
+
+const int ANCHO_DE_PANTALLA=1200;
+const int LARGO_DE_PANTALLA = 600;
+
+const int INICIAL_P1= 0;
+const int INICIAL_P2 = - (ANCHO_DE_PANTALLA * 2);
+
+
+
+
+class Fondo
+{
+
+public:
+	Fondo();
+	virtual ~Fondo();
+	int dibujarVentana();
+    void rellenarFondoNegro();
+	void dibujarFondo();
+	void actualizarFondo();
+	void cargarImagenFondo();
+	void init();
+	void mover(int velocidad);
+	int getPosicionRectFondo_x();
+
+	int getPosicionRect2Fondo_x();
+
+
+
+private:
+
+	void moverRectFondo_x(int num_de_pixeles);
+	void moverRect2Fondo_x(int num_de_pixeles);
+
+	SDL_Surface* imagen_fondo  = NULL ;
+	SDL_Surface* screen = NULL;
+	SDL_Rect rect_fondo;
+	SDL_Rect rect_fondo2;
+	int p1 = INICIAL_P1;
+	int p2 = INICIAL_P2;
+
+};
+
+
